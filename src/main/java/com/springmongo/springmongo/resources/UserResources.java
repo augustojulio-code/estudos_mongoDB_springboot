7fdda340e1821cae1b8189940b720910975b07fd
Julio <julio.mech@hotmail.com>
2023-05-15 08:43:53 -0300

    Return user posts endpoint

--- src/main/java/com/springmongo/springmongo/resources/UserResources.java
+++ src/main/java/com/springmongo/springmongo/resources/UserResources.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.springmongo.springmongo.domain.Post;
 import com.springmongo.springmongo.domain.User;
 import com.springmongo.springmongo.dto.UserDTO;
 import com.springmongo.springmongo.services.UserServices;
@@ -61,6 +62,12 @@ public class UserResources {
         obj.setId(id);
         obj = services.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = services.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
